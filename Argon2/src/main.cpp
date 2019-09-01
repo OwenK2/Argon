c5da49cd204ed0ea78fba620ea05a36b1fb0e49a
OwenK2 <opkuhn2011@gmail.com>
2019-09-01 10:47:43 -0700

    Edges

--- Argon2/src/main.cpp
+++ Argon2/src/main.cpp
@@ -1,12 +1,15 @@
 #include "argon.h"
+#include <cmath>
 
 int main(int argc, char* argv[]) {
 	Argon argon("Argon", 60, ARGON_BASIC|ARGON_IMAGES);
-	argon.addMouseListener(CLICK, [](Argon& a, Event& e) {
+	argon.setStroke(255,0,0);
+	argon.addMouseListener(CLICK, [](Argon& a, MouseEvent& e) {
 		Points pts;
-		pts.push_back({0,0});
-		pts.push_back({100,100});
-		pts.push_back({0,100});
+		pts.push_back({static_cast<int>(e.x + 30*cos(0)),static_cast<int>(e.y + 30*sin(0))});
+		pts.push_back({static_cast<int>(e.x + 30*cos(3*M_PI_4)),static_cast<int>(e.y + 30*sin(3*M_PI_4))});
+		pts.push_back({static_cast<int>(e.x + 30*cos(-3*M_PI_4)),static_cast<int>(e.y + 30*sin(-3*M_PI_4))});
+		a.polygon(pts);
 		a.strokePolygon(pts);
 	});
 	argon.start();
