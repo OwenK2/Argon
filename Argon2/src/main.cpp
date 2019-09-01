db36916cdff6c3b081ca9e07086be18584fb8190
Mike <michaelkuhn@Michaels-MacBook-Pro.local>
2019-09-01 10:59:26 -0700

    better key handling

--- Argon2/src/main.cpp
+++ Argon2/src/main.cpp
@@ -11,15 +11,34 @@ int main(int argc, char* argv[]) {
 	int y = 0;
 	int h = 50;
 	int w = 50;
+	bool arrowkey[4];
 	argon.addLoop([&](Argon& a) {
 		a.clear();
+		if(arrowkey[0]) {
+			y -= 3;
+		}
+		if(arrowkey[1]) {
+			x -= 2;
+		}
+		if(arrowkey[2]) {
+			y += 3;
+		}
+		if(arrowkey[3]) {
+			x += 3;
+		}
 		a.image("/Users/michaelkuhn/Downloads/IMG_1504.JPG", x,y,w,h);
 	});
 	argon.addKeyboardListener(KEYDOWN, [&](Argon& a, KeyboardEvent& e) {
-		if(strcmp(e.key,"W") == 0) {y -= 3;}
-		if(strcmp(e.key,"A") == 0) {x -= 3;}
-		if(strcmp(e.key,"S") == 0) {y += 3;}
-		if(strcmp(e.key,"D") == 0) {x += 3;}
+		if(strcmp(e.key,"W") == 0) {arrowkey[0] = true;}
+		if(strcmp(e.key,"A") == 0) {arrowkey[1] = true;}
+		if(strcmp(e.key,"S") == 0) {arrowkey[2] = true;}
+		if(strcmp(e.key,"D") == 0) {arrowkey[3] = true;}
+	});
+	argon.addKeyboardListener(KEYUP, [&](Argon& a, KeyboardEvent& e) {
+		if(strcmp(e.key,"W") == 0) {arrowkey[0] = false;}
+		if(strcmp(e.key,"A") == 0) {arrowkey[1] = false;}
+		if(strcmp(e.key,"S") == 0) {arrowkey[2] = false;}
+		if(strcmp(e.key,"D") == 0) {arrowkey[3] = false;}
 	});
 	argon.addMouseListener(CLICK, [](Argon& a, MouseEvent& e) {
 		Points pts;
