affa8e663d97294cdf41a431db0164a91d804eb9
Mike <michaelkuhn@Michaels-MacBook-Pro.local>
2019-09-01 12:51:06 -0700

    reorganizing, arrow controls added

--- Argon2/src/main.cpp
+++ Argon2/src/main.cpp
@@ -1,3 +1,6 @@
+
+
+
 #include "argon.h"
 #include <cmath>
 
@@ -9,36 +12,52 @@ int main(int argc, char* argv[]) {
 	argon.setStroke(255,255,255,255);
 	int x = 0;
 	int y = 0;
-	int h = 50;
-	int w = 50;
+	int h = -1;
+	int w = -1;
 	bool arrowkey[4];
 	argon.addLoop([&](Argon& a) {
 		a.clear();
-		if(a.keyboard.state[SDL_SCANCODE_UP]) {
+		// if(a.keyboard.state[SDL_SCANCODE_UP]) {
+		// 	y -= 3;
+		// }
+		// if(a.keyboard.state[SDL_SCANCODE_LEFT]) {
+		// 	x -= 3;
+		// }
+		// if(a.keyboard.state[SDL_SCANCODE_DOWN]) {
+		// 	y += 3;
+		// }
+		// if(a.keyboard.state[SDL_SCANCODE_RIGHT]) {
+		// 	x += 3;
+		// }
+
+		if(arrowkey[0]) {
 			y -= 3;
 		}
-		if(a.keyboard.state[SDL_SCANCODE_LEFT]) {
+		if(arrowkey[1]) {
 			x -= 3;
 		}
-		if(a.keyboard.state[SDL_SCANCODE_DOWN]) {
+		if(arrowkey[2]) {
 			y += 3;
 		}
-		if(a.keyboard.state[SDL_SCANCODE_RIGHT]) {
+		if(arrowkey[3]) {
 			x += 3;
 		}
-		a.image("/Users/owen/Documents/Coding/landing/midground.png", x,y);
+
+		a.image("/Users/michaelkuhn/Downloads/IMG_1504.jpg", x,y,w,h);
+
+		//a.image("/Users/owen/Documents/Coding/landing/midground.png", x,y,w,h);
 	});
 	argon.addKeyboardListener(KEYDOWN, [&](Argon& a, KeyboardEvent& e) {
-		if(strcmp(e.key,"W") == 0) {arrowkey[0] = true;}
-		if(strcmp(e.key,"A") == 0) {arrowkey[1] = true;}
-		if(strcmp(e.key,"S") == 0) {arrowkey[2] = true;}
-		if(strcmp(e.key,"D") == 0) {arrowkey[3] = true;}
+		if(strcmp(e.key,"W") == 0 || e.keyCode == 82) {arrowkey[0] = true;}
+		if(strcmp(e.key,"A") == 0 || e.keyCode == 80) {arrowkey[1] = true;}
+		if(strcmp(e.key,"S") == 0 || e.keyCode == 81) {arrowkey[2] = true;}
+		if(strcmp(e.key,"D") == 0 || e.keyCode == 79) {arrowkey[3] = true;}
 	});
 	argon.addKeyboardListener(KEYUP, [&](Argon& a, KeyboardEvent& e) {
-		if(strcmp(e.key,"W") == 0) {arrowkey[0] = false;}
-		if(strcmp(e.key,"A") == 0) {arrowkey[1] = false;}
-		if(strcmp(e.key,"S") == 0) {arrowkey[2] = false;}
-		if(strcmp(e.key,"D") == 0) {arrowkey[3] = false;}
+		if(strcmp(e.key,"W") == 0 || e.keyCode == 82) {arrowkey[0] = false;}
+		if(strcmp(e.key,"A") == 0 || e.keyCode == 80) {arrowkey[1] = false;}
+		if(strcmp(e.key,"S") == 0 || e.keyCode == 81) {arrowkey[2] = false;}
+		if(strcmp(e.key,"D") == 0 || e.keyCode == 79) {arrowkey[3] = false;}
 	});
 	// argon.addMouseListener(CLICK, [](Argon& a, Event& e) {
 	// 	Points pts;
