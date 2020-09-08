3e08596f144117f716af712aaef203586bd2988f
OwenK2 <opkuhn2011@gmail.com>
2020-09-08 14:20:36 -0700

    Working glfw window

--- src/argon.cpp
+++ src/argon.cpp
@@ -2,9 +2,30 @@
 
 
 Argon::Argon() {
+  if(!glfwInit()) {
+    printf("\e[31m[ERROR] Failed to initalize GLFW\e[0m\n");
+  }
+  
+  if(!(window = glfwCreateWindow(w, h, title, NULL, NULL))) {
+    printf("\e[31m[ERROR] Failed to create window\e[0m\n");
+    this->~Argon();
+  }
+  glfwMakeContextCurrent(window);
 
 }
 
 Argon::~Argon() {
+	glfwTerminate();
+}
 
+
+
+void Argon::start() {
+  while (!glfwWindowShouldClose(window)) {
+
+      glfwSwapBuffers(window);
+
+      /* Poll for and process events */
+      glfwPollEvents();
+  }
 }
--- src/argon.h
+++ src/argon.h
@@ -1,12 +1,26 @@
 #ifndef ARGON_H
 #define ARGON_H
 
+#include <cstdio>
+#include <GLFW/glfw3.h>
+
 
 class Argon {
 private:
+	//Window Properties
+	GLFWwindow* window;
+	const char* title = "Argon";
+	int32_t x = 0;
+	int32_t y = 0;
+	uint32_t w = 500;
+	uint32_t h = 500;
+
+
 public:
 	Argon();
 	~Argon();
+
+	void start();
 };
 
 #endif
